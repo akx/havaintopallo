2342e7e663231962350c6e4cd2aceb13d53d9cb5
Aarni Koskela <akx@iki.fi>
2020-03-06 16:00:11 +0200

    Embetter testing

--- tests/test_hp.py
+++ tests/test_hp.py
@@ -1,7 +1,13 @@
-from havaintopallo.conversion import convert_grid_xml, convert_compound_crs_unixtime
+from havaintopallo.conversion import convert_grid_xml, convert_compound_crs_unixtime, convert_measurement_xml
 
 
 def test_grid_conversion():
     with open("examples/grid_hirlam.xml") as infp:
         obs = list(convert_grid_xml(infp.read(), position_converter=convert_compound_crs_unixtime))
+        assert obs  # TODO: Improve test
+
+
+def test_point_conversion():
+    with open("examples/point_harmonie.xml") as infp:
+        obs = list(convert_measurement_xml(infp.read()))
         assert obs  # TODO: Improve test
